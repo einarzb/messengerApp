ff1c011d99bb35be5c5405f1a15a62b0bc834879
einarzb <einarzb@gmail.com>
2017-05-28 17:42:35 +0300

    added ng autocomplete and google maps api for first input

--- angular/public/js/app.js
+++ angular/public/js/app.js
@@ -1,18 +1,1 @@
 var app = angular.module('messengerApp', ['ui.router','ngAutocomplete']);
-
-// app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-//   $urlRouterProvider.otherwise('home');
-//   $locationProvider.html5Mode(true);
-//
-//    $stateProvider
-//   .state('home', {
-//       url: '/home',
-//       controller:'mainController',
-//       templateUrl: '/templates/partial-home.html'
-//   })
-//   .state('view', {
-//       url: '/view',
-//       controller:'mainController',
-//       templateUrl: '/templates/view.html'
-//   })
-});

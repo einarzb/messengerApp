6fb514382d96b82b5fed59b18434a7e943e4fb9a
Arkadi <mrarkadi@gmail.com>
2017-05-28 18:23:18 +0300

    arkadi commit first - added check origin vs destination by kilometers with distance_matrix_api

--- angular/public/js/mainController.js
+++ angular/public/js/mainController.js
@@ -1,6 +1,17 @@
-app.controller('mainController', function($scope, factory, $state) {
+app.controller('mainController', function($scope, $http, factory, $state) {
 
-
-
-
+    $scope.doBranchMagic = function() {
+      $http.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=HaPelech7,TelAviv-Yafo,Israel&destinations=Erez6,Lod,Israel&key=AIzaSyDTEUMGmlh_LdoOiHvOTgPhfmWobmyW3F4")
+        .then(function(response) {
+          if (response.data.rows[0].elements[0].status === "NOT_FOUND") { // NOTE: bad item that doesn't need to be counted
+            $scope.branches.splice(index, 1);
+            console.log("not found");
+          } else {
+            console.log(response.data);
+          }
+        }),
+        function errorCallback(response) {
+          console.log(response.data);
+        }
+    }
 });
